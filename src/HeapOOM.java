f05b908d9c9a02d59d70c90ea50f1dde64e41d8b
xiaoliang.yu <xiaoliang.yu.sh@dianping.com>
2016-10-30 16:44:12 +0800

    HeapOOM

---
+++ src/HeapOOM.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * Created by Jesse on 30/10/16.
+ */
+public class HeapOOM {
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true)
+            list.add(new OOMObject());
+    }
+}
