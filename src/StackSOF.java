6286e6a4ae5975c3b8e1c9b6d3cf3545d49168fc
xiaoliang.yu <xiaoliang.yu.sh@dianping.com>
2016-11-06 17:34:56 +0800

    StackSOF

---
+++ src/StackSOF.java
@@ -0,0 +1,21 @@
+/**
+ * VM Args: -Xss160k
+ * Created by Jesse on 6/11/16.
+ */
+public class StackSOF {
+    private int stackLength = 1;
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        StackSOF stackSOF = new StackSOF();
+        try {
+            stackSOF.stackLeak();
+        } catch (Throwable e) {
+            System.out.println(stackSOF.stackLength);
+            throw e;
+        }
+    }
+}
